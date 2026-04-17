729fc13c18ff5a4c48ab350598f6d2ecbe3ef550
Ilnur <gdlxesup@gmail.com>
2026-04-17 16:22:21 +0300

    counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
+  selector: 'app-root',
+  imports: [CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name = '';
+  message = 'hello';
 }
