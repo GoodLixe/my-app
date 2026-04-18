d6e0464fb05e021a06c53112645b747b53127889
Ilnur <gdlxesup@gmail.com>
2026-04-18 20:52:07 +0300

    1.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { CounterComponent } from './counter/counter.component';
+import { PlusFivePipe } from './plus-five.pipe';
 
 @Component({
-  standalone: true,
   selector: 'app-root',
-  imports: [CounterComponent],
+  standalone: true,
+  imports: [CommonModule, CounterComponent, PlusFivePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   message = 'hello';
+
+  users = [
+    { name: 'Ильнур', age: 20 },
+    { name: 'Алекс', age: 22 },
+    { name: 'Мария', age: 19 }
+  ];
+
+  today = new Date();
+  number = 10;
 }
